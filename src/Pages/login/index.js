1e0574e007dd31567d5aed0831e197613622128d
rashed <x.suwaidi@gmail.com>
2022-12-09 11:30:17 +0400

    add logo

--- src/Pages/login/index.js
+++ src/Pages/login/index.js
@@ -1,3 +1,5 @@
+import logo from '../../images/logo.jpeg'
+
 export default function Login(props) {
 
   const handleSubmit = (event) => {
@@ -16,9 +18,10 @@ export default function Login(props) {
     <>
       <section className=" dark:bg-gray-900">
         <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
-          <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <div  className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img class="w-10 h-10 mr-2" src={logo} alt="logo"/>
             UAE AI Ethics
-          </a>
+          </div>
           <div className="w-full bg-white rounded-lg border dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
             <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
               <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
